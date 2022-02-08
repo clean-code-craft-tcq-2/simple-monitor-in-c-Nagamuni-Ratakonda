60481e891a53e9ee51cf1d506c6269ec0624a434
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-02-08 22:05:35 +0530

    Update BMS.c

--- BMS.c
+++ BMS.c
@@ -12,7 +12,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate, int(*IsTemperatu
 int main() {
   assert(batteryIsOk(25, 70, 0.7,&IsTemperatureValid,&IsSOCValid,&IsChargeRateValid));
   assert(!batteryIsOk(50, 85, 0,&IsTemperatureValid,&IsSOCValid,&IsChargeRateValid));
-  assert(!batteryIsOk(45, 70, 0.7,&IsTemperatureValid,&IsSOCValid,&IsChargeRateValid));
+  assert(!batteryIsOk(46, 70, 0.7,&IsTemperatureValid,&IsSOCValid,&IsChargeRateValid));
   assert(!batteryIsOk(0, 70, 0.7,&IsTemperatureValid,&IsSOCValid,&IsChargeRateValid));
   assert(!batteryIsOk(10, 20, 0.7,&IsTemperatureValid,&IsSOCValid,&IsChargeRateValid));
   assert(!batteryIsOk(10, 80, 0.7,&IsTemperatureValid,&IsSOCValid,&IsChargeRateValid));
