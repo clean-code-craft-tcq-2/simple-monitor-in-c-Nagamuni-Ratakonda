930fbd8ec5ec963f2d30cf8e18facb9abbe60598
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-02-08 21:30:54 +0530

    Update BMS.c

--- BMS.c
+++ BMS.c
@@ -1,45 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define TEMPERATURE_MIN 0
-#define TEMPERATURE_MAX 45
-
-#define SOC_MIN 20
-#define SOC_MAX 80
-
-#define CHARGERATE_MAX 0.8
-
-int IsChargeRateValid(float chargeRate) {
-  if(chargeRate > CHARGERATE_MAX) {
-      printf("Charge Rate out of range!\n");
-      return 0;
-  }
-  else {
-    return 1;
-  }
-}
-
-int IsSOCValid(float soc) {
-  if(soc < SOC_MIN || soc > SOC_MAX) {
-      printf("State of Charge out of range!\n");
-      return 0;
-  }
-  else {
-    return 1;
-  }
-}
-
-int IsTemperatureValid(float temperature) {
-  if(temperature < TEMPERATURE_MIN || temperature > TEMPERATURE_MAX) {
-      printf("Temperature out of range!\n");
-      return 0;
-  }
-  else {
-    return 1;
-  }
-}
-
-
 int batteryIsOk(float temperature, float soc, float chargeRate) {
   int TemperatureCheck = IsTemperatureValid(temperature);
   int SOCCheck = IsSOCValid(soc);
