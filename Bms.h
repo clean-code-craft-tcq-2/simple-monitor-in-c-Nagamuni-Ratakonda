e01b35fba783662f2b1a78f2e3a894fa5d57a383
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-02-08 22:00:23 +0530

    Update Bms.h

--- Bms.h
+++ Bms.h
@@ -9,4 +9,4 @@
 int IsChargeRateValid(float chargeRate);
 int IsSOCValid(float soc);
 int IsTemperatureValid(float temperature);
-int batteryIsOk(float temperature, float soc, float chargeRate);
+int batteryIsOk(float temperature, float soc, float chargeRate, int(*IsTemperatureValid_FuncPtr)(float), int(IsSOCValid_FuncPtr)(float), int(*IsChargeRateValid_FncPtr)(float));
