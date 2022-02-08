59b6694271eceddce5d59c07c4a53d00ce17db85
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-02-08 18:19:00 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,18 +1,50 @@
 #include <stdio.h>
 #include <assert.h>
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
+#define TEMPERATURE_MIN 0
+#define TEMPERATURE_MAX 45
+
+#define SOC_MIN 20
+#define SOC_MAX 80
+
+#define CHARGERATE_MAX 0.8
+
+int IsChargeRateValid(float chargeRate) {
+  if(chargeRate > CHARGERATE_MAX) {
+      printf("Charge Rate out of range!\n");
+      return 0;
   }
-  return 1;
+  else {
+    return 1;
+  }
+}
+
+int IsSOCValid(float soc) {
+  if(soc < SOC_MIN || soc > SOC_MAX) {
+      printf("State of Charge out of range!\n");
+      return 0;
+  }
+  else {
+    return 1;
+  }
+}
+
+int IsTemperatureValid(float temperature) {
+  if(temperature < TEMPERATURE_MIN || temperature > TEMPERATURE_MAX) {
+      printf("Temperature out of range!\n");
+      return 0;
+  }
+  else {
+    return 1;
+  }
+}
+
+
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+  int TemperatureCheck = IsTemperatureValid(temperature);
+  int SOCCheck = IsSOCValid(soc);
+  int ChargeRateCheck = IsChargeRateValid(chargeRate);
+  return (TemperatureCheck && SOCCheck && ChargeRateCheck);
 }
 
 int main() {
