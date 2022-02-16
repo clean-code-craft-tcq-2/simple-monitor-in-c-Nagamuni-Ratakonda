e32888879c4558a31e077c1818453e5d1e3ad9be
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-02-16 17:00:59 +0530

    Update BMS.c

--- BMS.c
+++ BMS.c
@@ -2,10 +2,41 @@
 #include <assert.h>
 #include "Bms.h"
 
-int batteryIsOk(float temperature, float soc, float chargeRate, EarlyWarningForBatteryParameters ParamWithEarlyWarning, int(*IsTemperatureValid_FuncPtr)(float), int(IsSOCValid_FuncPtr)(float), int(*IsChargeRateValid_FncPtr)(float)){
-  int TemperatureCheck = IsTemperatureValid_FuncPtr(temperature,ParamWithEarlyWarning);
-  int SOCCheck = IsSOCValid_FuncPtr(soc,ParamWithEarlyWarning);
-  int ChargeRateCheck = IsChargeRateValid_FncPtr(chargeRate,ParamWithEarlyWarning);
+PrintEarlyWarningForLowTolerance(int BatteryParameter)
+{
+  printf("Lower Tolerance limit approached for");
+}
+
+bool IsToleranceLowerLimitApproached(int BatteryParameter, int BatteryParamLowerLimit)
+{
+  float lowerToleranceValue = getLowerToleranceValue(BatteryParameter,BatteryParamLowerLimit);
+  
+  if(lowerToleranceValue == (BatteryParamLowerLimit+lowerToleranceValue))
+  {
+    PrintEarlyWarningForLowTolerance(BatteryParameter);
+    return false;
+  }
+  
+  return true;
+}
+
+bool CheckForEarlyWarning(int BatteryParameter, int BatteryParamLowerLimit, int BatteryParamHigherLimit, EarlyWarningForBatteryParameters ParamWithEarlyWarning)
+{
+  int EarlyWarning = false;
+  
+  EarlyWarning = IsToleranceLowerLimitApproached(BatteryParameter,BatteryParamLowerLimit);
+  if(!EarlyWarning)
+  {
+    EarlyWarning = IsToleranceUpperLimitApproached(BatteryParameter,BatteryParamHigherLimit);
+  }
+  
+  return EarlyWarning;
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate, EarlyWarningForBatteryParameters ParamWithEarlyWarning, int(*IsTemperatureValid_FuncPtr)(float), int(IsSOCValid_FuncPtr)(float), int(*IsChargeRateValid_FncPtr)(float)){
+  bool TemperatureCheck = IsTemperatureValid_FuncPtr(temperature,ParamWithEarlyWarning);
+  bool SOCCheck = IsSOCValid_FuncPtr(soc,ParamWithEarlyWarning);
+  bool ChargeRateCheck = IsChargeRateValid_FncPtr(chargeRate,ParamWithEarlyWarning);
   return (TemperatureCheck && SOCCheck && ChargeRateCheck,ParamWithEarlyWarning);
 }
 
