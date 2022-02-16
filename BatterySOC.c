9e414499aeeeb4fb6d5ff623df0c214a266d62e3
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-02-16 15:10:01 +0530

    Update BatterySOC.c

--- BatterySOC.c
+++ BatterySOC.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "Bms.h"
 
-int IsSOCValid(float soc) {
+int IsSOCValid(float soc, EarlyWarningForBatteryParameters ParamWithEarlyWarning) {
   if(soc < SOC_MIN || soc > SOC_MAX) {
       printf("State of Charge out of range!\n");
       return 0;
