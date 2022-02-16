2f0afad74dc2a9ad069697f8267793cd54318ff0
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-02-16 15:09:43 +0530

    Update BatteryChargeRate.c

--- BatteryChargeRate.c
+++ BatteryChargeRate.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "Bms.h"
 
-int IsChargeRateValid(float chargeRate) {
+int IsChargeRateValid(float chargeRate, EarlyWarningForBatteryParameters ParamWithEarlyWarning) {
   
   if(chargeRate > CHARGERATE_MAX )
   {
