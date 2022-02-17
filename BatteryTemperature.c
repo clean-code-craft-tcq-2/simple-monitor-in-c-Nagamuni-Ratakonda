9fa2821fa99ffbf6360edb7ba233b4fcff1325b7
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-02-17 08:34:10 +0530

    Update BatteryTemperature.c

--- BatteryTemperature.c
+++ BatteryTemperature.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include "Bms.h"
 
-bool IsTemperatureValid(float temperature, EarlyWarningForBatteryParameters ParamWithEarlyWarning) {
+bool IsTemperatureValid(float temperature) {
   if(temperature < TEMPERATURE_MIN || temperature > TEMPERATURE_MAX) {
       printf("Temperature out of range!\n");
       return false;
   }
   
   return true;
+}
+
+bool BatteryTemperature(float temperature, EarlyWarningForBatteryParameters ParamWithEarlyWarning){
+  bool batteryTemperatureResult = false;
+  
+  batteryTemperatureResult = IsTemperatureValid(temperature);
+  
+  if(batteryTemperatureResult)
+  {
+    if(ParamWithEarlyWarning != WarningForNone)
+    {
+      batteryTemperatureResult = CheckForEarlyWarning(BATTERYPARAMETER_TEMPERATURE, temperature);
+    }
+  }
+  
+  return batteryTemperatureResult;
 }
